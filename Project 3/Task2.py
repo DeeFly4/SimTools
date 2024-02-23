49a9d8db5da42ac9bb1ed4624cc6fe5781c649f9
DeeFly <daniel.holm@majodang.se>
2024-02-23 15:49:04 +0100

    Added task2 file and .gitignore for pycache

--- Project 3/Task2.py
+++ Project 3/Task2.py
@@ -1,32 +1,27 @@
-from assimulo.problem import Explicit_Problem
+from classes import Explicit_Problem_2nd
 from assimulo.solvers import CVode
 import numpy as np
-from numpy import multiply, divide, reciprocal, hstack
-
 import matplotlib.pyplot as mpl
 
-class Explicit_Problem_2nd(Explicit_Problem):
-	def __init__(self, M, C, K, f, u0, udot0, t0):
-		self.a = -divide(K,M)
-		self.b = -divide(C,M)
-		self.c = reciprocal(M)
-		self.f = f
-		Explicit_Problem.__init__(self, self.rhs, hstack((u0, udot0)), t0)
-
-	def rhs(self, t, y):
-		u = y[0:len(y)//2]
-		udot = y[len(y)//2:len(y)]
-  
-		y1dot = udot
-		y2dot = multiply(self.a, u) + multiply(self.b, udot) + multiply(self.c, self.f(t))
-  
-		return hstack((y1dot, y2dot))
-
 def f(t):
-    return 0.
+	return 0
 
-testeq = Explicit_Problem_2nd(1., 3., .2, f, 1., 0., 0.)
-sim = CVode(testeq)
-sim.simulate(5)
+M = 1
+C = 3
+K = 2
 
-sim.plot()
+u0 = 1
+up0 = 1
+
+t0 = 0
+tf = 5
+
+model = Explicit_Problem_2nd(M, C, K, f, u0, up0, t0)
+sim = CVode(model)
+
+t, u_all = sim.simulate(tf)
+u = [states[0:len(states)//2] for states in u_all]
+
+mpl.plot(t, u)
+
+mpl.show()
