5a8ea71fc4f6cd99f06ce4e1165c190f50e1b53c
DeeFly <daniel.holm@majodang.se>
2024-03-04 18:29:05 +0100

    Done with this (copium)

--- Project 3/Task23.py
+++ Project 3/Task23.py
@@ -12,33 +12,44 @@ def f2(t):
 	return np.array([0, 0, 50*np.sin(omega*t)]) if t < np.pi / omega else np.zeros(3,)
 
 M1 = np.diag([1, 1])
-K1 = np.array([[1, -2], [3, -4]])
-C1 = np.array([[-2, 0], [-1, 0]])
+K1 = np.array([[1, 1], [2, -3]])
+C1 = np.array([[2, 0], [-1, 0]])
 
 M2 = np.diag([10, 20, 30])
 K2 = 1e3 * np.array([[45, -20, -15], [-20, 45, -25], [-15, -25, 40]])
 C2 = 3e-2 * K2
 
-u0 = np.ones(2,)
-up0 = np.zeros(2,)
+u0 = np.zeros(3,)
+up0 = np.zeros(3,)
 
 t0 = 0
-tf = 1
+tf = 3
 
-model = Explicit_Problem_2nd(M1, C1, K1, f1, u0, up0, t0)
+model = Explicit_Problem_2nd(M2, C2, K2, f2, u0, up0, t0)
+cvode = CVode(model)
 sim = Newmark(model)
 sim.h = 1e-3
 
-t, u_all = sim.simulate(tf)
-u = [states[0:len(states)//2] for states in u_all]
+t1, u_all = cvode.simulate(tf)
+t2, u2 = sim.simulate(tf)
+u1 = [states[0:len(states)//2] for states in u_all]
 
-mpl.plot(t, u_all, lw=2)
-mpl.hlines(0, 0, tf, ls='--', colors='k')
-# mpl.axis([0, tf, -5, 5])
+fig, (ax1, ax2) = mpl.subplots(1,2)
 
-mpl.title('Second-order equation system using Newmark', fontsize=14)
-mpl.xlabel('Time [s]', fontsize=14)
+ax1.plot(t1, u1, lw=2)
+ax2.plot(t2, u2, lw=2)
+ax1.hlines(0, 0, tf, ls='--', colors='k')
+ax2.hlines(0, 0, tf, ls='--', colors='k')
 
-mpl.grid(True)
+ax1.set_title('CVode', fontsize=16)
+ax2.set_title('Newmark', fontsize=16)
+ax1.set_xlabel('Time [s]', fontsize=14)
+ax2.set_xlabel('Time [s]', fontsize=14)
+
+ax1.axis([0, tf, -0.005, 0.01])
+ax2.axis([0, tf, -0.005, 0.01])
+
+ax1.grid(True)
+ax2.grid(True)
 
 mpl.show()
